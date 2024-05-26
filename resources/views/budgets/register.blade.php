71803b1b314798eab22f29fe4f1ffc348a974725
joaoenrique <jebsantosalves@gmail.com>
2024-05-26 19:31:11 -0300

    fix: resize forms to mobile

--- resources/views/budgets/register.blade.php
+++ resources/views/budgets/register.blade.php
@@ -51,7 +51,7 @@
                             <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
                           </svg>
                         </div>
-                        <input value="{{ old('email') }}" type="text" name="email" id="email" class="@error('email') bg-red-50 border border-red-500 text-red-900 placeholder-red-700 @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com">
+                        <input value="{{ old('email') }}" type="text" name="email" id="email" class="@error('email') bg-red-50 border border-red-500 text-red-900 placeholder-red-700 @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com">
 
                     </div>
                     @error('email')
